5003f3a078e0dc274743390337a06bda277b04ff
Aftab Ali <aliaftab612@gmail.com>
2023-06-05 01:53:58 +0530

    chnages

--- controllers/authController.js
+++ controllers/authController.js
@@ -91,7 +91,7 @@ exports.login = async (req, res, next) => {
       secure: process.env.NODE_ENV === 'production' ? true : false,
       maxAge: process.env.JWT_EXPIRES_IN * 1000,
       priority: 'high',
-      domain: '.onrender.com',
+      domain: 'onrender.com',
     });
 
     res.cookie('isAuthenticated', true, {
@@ -100,7 +100,7 @@ exports.login = async (req, res, next) => {
       secure: process.env.NODE_ENV === 'production' ? true : false,
       maxAge: process.env.JWT_EXPIRES_IN * 1000,
       priority: 'high',
-      domain: '.onrender.com',
+      domain: 'onrender.com',
     });
 
     res.status(200).json({
