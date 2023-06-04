fd59464254d2c0c49037b394715c06a088d06890
Aftab Ali <aliaftab612@gmail.com>
2023-06-05 01:17:22 +0530

    dscds

--- controllers/authController.js
+++ controllers/authController.js
@@ -91,7 +91,7 @@ exports.login = async (req, res, next) => {
       secure: process.env.NODE_ENV === 'production' ? true : false,
       maxAge: process.env.JWT_EXPIRES_IN * 1000,
       priority: 'high',
-      domain: '',
+      domain: '.web.app',
     });
 
     res.cookie('isAuthenticated', true, {
@@ -100,7 +100,7 @@ exports.login = async (req, res, next) => {
       secure: process.env.NODE_ENV === 'production' ? true : false,
       maxAge: process.env.JWT_EXPIRES_IN * 1000,
       priority: 'high',
-      domain: '',
+      domain: '.web.app',
     });
 
     res.status(200).json({
