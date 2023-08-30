4d6050d41bed4ce1278e2d2a83f3db67a6e1ec29
Aftab Ali <aliaftab612@gmail.com>
2023-08-30 21:21:38 +0530

    improved email exists response

--- utility/exceptionHandler.js
+++ utility/exceptionHandler.js
@@ -1,4 +1,12 @@
 exports.handleException = (exception, res) => {
+  if (exception.code && exception.code === 11000) {
+    res.status(409).json({
+      status: 'fail',
+      message: 'Email Already Exists',
+    });
+    return;
+  }
+
   res.status(500).json({
     status: 'exception',
     message:
