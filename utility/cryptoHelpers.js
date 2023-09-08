845df82aa5f803ff3e0151b212a8c48152701c5e
Aftab Ali <aliaftab612@gmail.com>
2023-09-08 23:58:50 +0530

    used pbkdf2 async encryption algo

--- utility/cryptoHelpers.js
+++ utility/cryptoHelpers.js
@@ -1,11 +1,18 @@
 const crypto = require('crypto');
+const util = require('util');
 
 exports.performExtraHashingOnLoginHash = async (loginHash, salt) => {
   const hashIterations = parseInt(
     process.env.HASH_ITERATIONS ? 600000 : process.env.HASH_ITERATIONS
   );
 
-  return crypto
-    .pbkdf2Sync(loginHash, salt, hashIterations, 32, 'sha256')
-    .toString('hex');
+  return (
+    await util.promisify(crypto.pbkdf2)(
+      loginHash,
+      salt,
+      hashIterations,
+      32,
+      'sha256'
+    )
+  ).toString('hex');
 };
